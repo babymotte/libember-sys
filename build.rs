9b90113154634d25075e95d8ce2ffceca528d0c6
Michael Bachmann <mbachmann@bbmsoft.net>
2022-06-01 17:39:53 +0200

    code hygene
    building everything automatically from build.rs
    all downloaded/generated artifacts are put into target dir

--- build.rs
+++ build.rs
@@ -1,13 +1,41 @@
-extern crate bindgen;
-
-use std::path::PathBuf;
+use duckscript::{runner, types::runtime::Context};
+use std::{env, fs, path::PathBuf};
 
 fn main() {
-    // Tell cargo to tell rustc to link the ember_slim-shared library.
-    println!("cargo:rustc-link-lib=ember_slim-shared");
+    let mut context = Context::new();
+    duckscriptsdk::load(&mut context.commands).expect("Error setting up duckscript environment.");
+    runner::run_script(
+        r#"
+    root = get_env CARGO_MANIFEST_DIR
+    target = get_env OUT_DIR
+    
+    if not is_path_exists ${root}/target/ember-plus
+        exec git clone https://github.com/Lawo/ember-plus.git ${root}/target/ember-plus
+    else
+        echo ${root}/target/ember-plus already exists.
+    end
+    cd ${root}/target/ember-plus
+    exec git config advice.detachedHead false
+    exec git checkout v1.8.2.1
+    cd ${target}
+    if not is_path_exists ${target}/libember_slim/libember_slim-shared.so
+        exec cmake ${root}/target/ember-plus
+        exec make
+    else
+        echo ${target}/libember_slim/libember_slim-shared.so already exists.
+    end
+    cd ${root}
+    "#,
+        context,
+    )
+    .expect("Compiling Ember+ failed!");
 
-    // Tell cargo to invalidate the built crate whenever the wrapper changes
-    println!("cargo:rerun-if-changed=ember-plus/libember_slim/Source/*");
+    let root = env::var("CARGO_MANIFEST_DIR").expect("failed to get manifest dir");
+    let out = env::var("OUT_DIR").expect("failed to get out dir");
+
+    // Tell cargo to tell rustc to link the ember_slim-shared library.
+    println!("cargo:rustc-link-search=native={}/libember_slim/", out);
+    println!("cargo:rustc-link-lib=ember_slim-shared");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -15,7 +43,10 @@ fn main() {
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
-        .header("ember-plus/libember_slim/Source/emberplus.h")
+        .header(format!(
+            "{}/target/ember-plus/libember_slim/Source/emberplus.h",
+            root
+        ))
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
@@ -25,7 +56,10 @@ fn main() {
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from("generated");
+    let out_path = PathBuf::from(out).join("generated");
+    if !out_path.is_dir() {
+        fs::create_dir(&out_path).expect("could not create output directory");
+    }
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
